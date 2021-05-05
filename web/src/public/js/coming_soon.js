b7ddfbbd5c016cbff5d24a3da0ca704eb6c5bc68
kimsternator <sskim@ucsd.edu>
2021-05-05 15:44:30 -0700

    Initial commit

---
+++ web/src/public/js/coming_soon.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+  var theDate = new Date(2021, 4, 10);
+  var day = 86400;
+  var hour = 3600;
+  var minute = 60;
+
+  $(function count() {
+    var note = $('#note'),
+        ts = new Date(new Date($.now()));
+
+    var seconds = Math.round((theDate.getTime() - ts.getTime()) / 1000);
+    var days = Math.floor(seconds / day);
+    seconds = seconds - days * day;
+    var hours = Math.floor(seconds / hour);
+    seconds = seconds - hours * hour;
+    var minutes = Math.floor(seconds / minute);
+    seconds = seconds - minutes * minute;
+    $("#countdown").text(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
+
+   setInterval(count, 1000);
+  });
+});
+

1550fbcf3e5d61dd32b78f17979282c680e2fbcb
kimsternator <sskim@ucsd.edu>
2021-05-26 19:18:36 -0700

    Edit font + update coming soon

--- web/src/public/js/coming_soon.js
+++ web/src/public/js/coming_soon.js
@@ -9,12 +9,21 @@ $( document ).ready(function() {
         ts = new Date(new Date($.now()));
 
     var seconds = Math.round((theDate.getTime() - ts.getTime()) / 1000);
-    var days = Math.floor(seconds / day);
-    seconds = seconds - days * day;
-    var hours = Math.floor(seconds / hour);
-    seconds = seconds - hours * hour;
-    var minutes = Math.floor(seconds / minute);
-    seconds = seconds - minutes * minute;
+    var days, hours, minutes;
+    if(seconds <= 0) {
+      days = Math.floor(seconds / day);
+      seconds = seconds - days * day;
+      hours = Math.floor(seconds / hour);
+      seconds = seconds - hours * hour;
+      minutes = Math.floor(seconds / minute);
+      seconds = seconds - minutes * minute;
+    }
+    else {
+      days = 0;
+      hours = 0;
+      seconds = 0;
+    }
+
     $("#countdown").text(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
 
    setInterval(count, 1000);
